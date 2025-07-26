c9e924c84da2bdc43f4a004fb52f0b42b98f9464
Shahbaz <shahbaz.khan.wip@gmail.com>
2025-07-26 15:25:39 +0500

    add cache selected currecny

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,14 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 use Msdevbytes\CurrencySwitcher\Services\CurrencyRateService;
 
 Route::middleware(['web'])->group(function () {
     Route::post('/currency-switcher', function (\Illuminate\Http\Request $request) {
         $request->validate(['currency' => 'required|string']);
-
+        Cache::set('currency', $request->get('currency'));
         session()->put('currency', $request->get('currency'));
 
         return back();
