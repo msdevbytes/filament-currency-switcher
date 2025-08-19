acfaa2f5ba2b953a96fe4ea93a154879d076f0f1
Shahbaz <shahbaz.khan.wip@gmail.com>
2025-08-19 16:17:08 +0500

    fix cache issue and make is user base

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 use Msdevbytes\CurrencySwitcher\Services\CurrencyRateService;
@@ -8,7 +9,7 @@ use Msdevbytes\CurrencySwitcher\Services\CurrencyRateService;
 Route::middleware(['web'])->group(function () {
     Route::post('/currency-switcher', function (\Illuminate\Http\Request $request) {
         $request->validate(['currency' => 'required|string']);
-        Cache::set('currency', $request->get('currency'));
+        Cache::set(Auth::user()?->id . '-currency', $request->get('currency'));
         session()->put('currency', $request->get('currency'));
 
         return back();
